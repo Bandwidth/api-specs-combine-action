333ebe37990f76e75a2deb265051dfc655097aec
ckoegel <ckoegel1006@gmail.com>
2023-06-12 17:30:14 -0400

    path

--- test/cli.test.js
+++ test/cli.test.js
@@ -31,7 +31,7 @@
     it("should run the cli with no errors", async () => {
         const consoleSpy = jest.spyOn(console, "log");
 
-        await runCommand("--config", "./fixtures/oas-merge-config.yml", "--test");
+        await runCommand("--config", "./test/fixtures/oas-merge-config.yml", "--test");
 
         expect(consoleSpy).toBeCalledWith("Merge successful!");
     });
