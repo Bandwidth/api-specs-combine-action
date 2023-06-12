583f3fb52b6f7233c4f9502f98ecf5a6a6380f83
ckoegel <ckoegel1006@gmail.com>
2023-06-12 17:21:18 -0400

    SWI-2821 Migrate to Internal Action

---
+++ cli.test.js
@@ -0,0 +1,38 @@
+/**
+ * Programmatically set arguments and execute the CLI script
+ *
+ * @param {...string} args - positional and option arguments for the command to run
+ */
+ async function runCommand(...args) {
+    process.argv = [
+      "node",
+      "index.js",
+      ...args,
+    ];
+  
+    return require("./index.js");
+  }
+
+  describe("cli", () => {
+    let originalArgv;
+  
+    beforeEach(() => {
+      jest.resetModules();
+  
+      originalArgv = process.argv;
+    });
+  
+    afterEach(() => {
+      jest.resetAllMocks();
+  
+      process.argv = originalArgv;
+    });
+
+    it("should run the cli with no errors", async () => {
+        const consoleSpy = jest.spyOn(console, "log");
+
+        await runCommand("--config", "./oas-merge-config.yml", "--test");
+
+        expect(consoleSpy).toBeCalledWith("Merge successful!");
+    });
+});
---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const { merge, isErrorResult } = require('openapi-merge');
+const argv = require('minimist')(process.argv.slice(2));
+
+function buildSpecArray(config) {
+  const mergeArray = [];
+  for (i in config.inputs) {
+    let apiSpec = yaml.load(
+      fs.readFileSync(configFile.inputs[i].inputFile, 'utf8')
+    );
+    let servers = apiSpec.servers;
+
+    for (specPath in apiSpec.paths) {
+      apiSpec.paths[specPath].servers = servers;
+    }
+
+    delete apiSpec.info;
+    delete apiSpec.servers;
+
+    mergeObj = {
+      oas: apiSpec,
+    };
+
+    mergeArray.push(mergeObj);
+  }
+  return mergeArray;
+}
+
+function mergeSpecs(mergeArray) {
+  const mergeResult = merge(mergeArray);
+  if (isErrorResult(mergeResult)) {
+    console.error(`${mergeResult.message} (${mergeResult.type})`);
+  } else {
+    console.log(`Merge successful!`);
+    // console.log(JSON.stringify(mergeResult.output, null, 2));
+    return mergeResult;
+  }
+}
+
+function main(config) {
+  const mergeArray = buildSpecArray(config);
+  const apiSpec = mergeSpecs(mergeArray).output;
+
+  let info = {
+    title: 'Bandwidth',
+    description: `Bandwidth's Communication APIs`,
+    contact: {
+      name: 'Bandwidth',
+      url: 'https://dev.bandwidth.com',
+      email: 'letstalk@bandwidth.com',
+    },
+    version: '1.0.0',
+  };
+  apiSpec.info = info;
+
+  /**
+   * Only save the file if the -t/--test flag is not present
+   */
+  fileExtension = path.extname(configFile.output);
+  if( argv.t != true && argv.test != true ){
+    if(fileExtension == '.json'){
+      fs.writeFileSync(configFile.output, JSON.stringify(apiSpec, null, 4), 'utf8');
+    } else if(fileExtension == '.yaml' || fileExtension == '.yml'){
+      fs.writeFileSync(configFile.output, yaml.dump(apiSpec));
+    } else {
+      throw new Error('Unsupported output file type. Only `.json`, `.yaml`, and `.yml` are supported.');
+    }
+  }
+}
+
+const configFile = yaml.load(fs.readFileSync(argv.c || argv.config, 'utf8'));
+main(configFile);
